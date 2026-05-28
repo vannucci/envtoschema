639b3d13db75914496a16d3f6a5d2921d17b5940
Michael A Vannucci <vannucci@gmail.com>
2026-05-28 00:05:43 +0000

    added handlers

--- internal/server/server.go
+++ internal/server/server.go
@@ -8,3 +8,22 @@ type FieldView struct {
 type PageData struct {
 	Fields []FieldView
 }
+
+func Start(elements []InferredElement) {
+	fields := toFieldViews(elements)
+	data := PageData{Fields: fields}
+
+	http.HandleFunc("GET /", indexHandler(data))
+	http.HandleFunc("POST /generate", generateHandler)
+
+	log.Println("listening on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func toFieldViews(elements []InferredElement) []FieldView {
+	views := make([]FieldView, 0, len(elements))
+	for _, e := range elements {
+		views = append(views, toFieldView(e))
+	}
+	return views
+}
